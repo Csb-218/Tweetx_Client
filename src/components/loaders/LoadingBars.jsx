d2bd276cc415b3c893cc144c1788dfa949f9a9a1
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2024-01-24 01:10:13 +0530

    feat : added loading animation

---
+++ src/components/loaders/LoadingBars.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoadingBars = () => {
+  return (
+    <span className="loading loading-bars loading-lg my-16"/>
+  )
+}
+
+export default LoadingBars
---
+++ src/components/loaders/LoadingSpinner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoadingSpinner = () => {
+  return (
+    <span className="loading loading-spinner loading-lg absolute top-1/3 left-1/2"></span>
+  )
+}
+
+export default LoadingSpinner
