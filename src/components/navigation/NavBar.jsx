8fa7ff941d0741e062351b4b9ed2d885737781e1
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2024-01-24 18:50:52 +0530

    fix : dropdown now closes on click

--- src/components/navigation/NavBar.jsx
+++ src/components/navigation/NavBar.jsx
@@ -33,10 +33,10 @@ const NavBar = () => {
         <div className={hide?"hidden":'block relative'}>
              <div className="navbar fixed z-50 bg-white top-0 flex flex-row justify-between shadow-md">
             <div className="navbar-start ">
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                <details className="dropdown">
+                    <summary tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </div>
+                    </summary>
                     <ul tabIndex={0} className="menu dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                         {pages?.map(page => {
                             return (
@@ -50,7 +50,7 @@ const NavBar = () => {
                             )
                         })}
                     </ul>
-                </div>
+                </details>
                 <a className="btn btn-ghost text-xl text-rose-400" href='feed'>TweetX</a>
             </div>
             <div className="navbar-center hidden lg:flex ">
