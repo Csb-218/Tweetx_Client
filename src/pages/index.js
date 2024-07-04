2b4fd5f1ae1078c7f4ee38e8d3ab410ba5db5fb5
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2024-07-04 18:39:49 +0530

    feat : feed accessible on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,86 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import {Feed} from '../components/feed/Feed'
-const inter = Inter({ subsets: ["latin"] });
+import { useState, useRef } from 'react'
+import { useAuth } from '@/context/AuthContext';
+import { TweetXCard, UserXCard } from '@/components/containers';
+import { getUserFeed, createPost } from '@/services/services';
+import { FormCreatePost } from '@/components/forms';
+
+import { useMutation, useQuery } from 'react-query';
+import LoadingBars from '@/components/loaders/LoadingBars';
+import LoadingSpinner from '@/components/loaders/LoadingSpinner';
 
 export default function Home() {
+  const formRef = useRef();
+  const userObject = useAuth();
+
+  // const [tweets, setTweets] = useState([])
+
+  const token = userObject?.user?.token
+
+  const { isLoading, mutate: addPost } = useMutation({
+    mutationFn: (data) => {
+      return createPost(token, data)
+    },
+    onSuccess: (res) => {
+      refetch()
+      formRef?.current?.reset()
+    },
+    onError: (error) => {
+      console.error('create post error', error)
+    }
+  })
+
+  const { isLoading: tweetsLoading,data:tweetsResponse, isSuccess,isRefetching,refetch } = useQuery({
+    queryKey: ['tweets'],
+    queryFn:()=>getUserFeed(token),
+    onSuccess: (res => {
+      console.log(res)
+    }),
+    onError:(err)=>{
+      console.error(err)
+    }
+  })
+
+  const tweets = tweetsResponse?.data?.feed
+  // console.log(tweets,99,process.env.NEXT_PUBLIC_BASE_URL,isLoading)
+
   return (
-    <Feed/>
-  );
+
+    <div className=' flex flex-col items-center'>
+
+      <FormCreatePost addPost={addPost} isLoading={isLoading} ref={formRef} />
+
+      {/* <CreatePostModal addPost={addPost} isLoading={isLoading}/> */}
+      <div className=' flex justify-center  lg:w-10/12  w-11/12'>
+
+
+        {
+          tweetsLoading ?
+            <LoadingSpinner/>
+            :
+            isSuccess ?
+            tweets?.length > 0 ?
+                <div>
+                  {
+                    tweets?.map((tweet, index) => {
+                      return (
+                        <TweetXCard key={index} tweet={tweet} />
+                      )
+                    })
+                  }
+                </div>
+                :
+                <div>
+                  Follow some users to see your feed.
+                </div>
+              :
+              'Could not fetch feed posts !'
+
+        }
+
+      </div>
+
+    </div>
+
+
+  )
 }
